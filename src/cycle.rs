9f2d9584e14b28694f4b362d980c8bfcd2f11473
Ingervo Eliel E O <ebingerv@turso02.cs.helsinki.fi>
2024-01-17 13:55:26 +0200

    Debugging

--- src/cycle.rs
+++ src/cycle.rs
@@ -91,15 +91,15 @@ fn longest_subwalk(cycle: &Vec<Edge>, index1: EdgeId, index2: EdgeId, weight: We
             one_cycle.push_back(edge.clone());
             weight_left = weight;
             extra_weight = weight_left;
-            //if edge.id == last_edge_of_cycle.id {
-            //    break;
-            //}
+            if edge.id == last_edge_of_cycle.id {
+                break;
+            }
         } else {
             // Adjusting the indice for the the next round
             if index2 == index1 && one_cycle.len() < 2 {
                 index2 = get_next_index(index2, cycle);
             }
-            else if index2 != get_next_index(index1, cycle) && one_cycle.len() < cycle.len() {
+            else if index2 != get_next_index(index1, cycle) {
                 index2 = get_former_index(index2, cycle);
             }
             break;
